417222090beadfed18a8cb1d4e8229d62e9f8a4d
SankalpPyasi <sankalppyasi6@gmail.com>
2025-02-22 18:37:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+    const line = document.getElementById('line');
+    let angle = 0;
+
+    setInterval(() => {
+        angle += 2;
+        line.style.transform = `rotate(${angle}deg)`;
+    }, 20);
+});
